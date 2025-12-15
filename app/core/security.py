09caaf96cadc8593fe27bd1e9ea53a9eaba79f08
Семён Селиванов <semaselivan@gmail.com>
2025-12-15 08:25:58 +0500

    Dev fix

--- app/core/security.py
+++ app/core/security.py
@@ -5,5 +5,6 @@ config = AuthXConfig()
 config.JWT_SECRET_KEY = settings.JWT_SECRET_KEY
 config.JWT_TOKEN_LOCATION = ["cookies"]
 config.JWT_ACCESS_COOKIE_NAME = "access_token"
+config.JWT_COOKIE_CSRF_PROTECT = False
 
 security = AuthX(config=config)
